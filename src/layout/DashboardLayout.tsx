cc10b4f98ecd96e4703af74a6f0d73f21fb4d226
Thu Nguyen <thuthien2001@gmail.com>
2025-04-25 11:33:32 +0700

    feat: dynamic title using data from sidebar

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { Outlet } from "react-router";
 import {
   SidebarInset,
@@ -10,17 +12,29 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function DashboardLayout() {
+  const [title, setTitle] = useState("Ripperdoc Clinic");
+
+  const handleTitleChange = (newTitle: string) => {
+    setTitle(newTitle);
+  };
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SidebarProvider>
-        <AppSidebar />
+        <AppSidebar sendTitle={handleTitleChange} />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <ModeToggle />
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+            <div className="flex items-center h-full">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <h1 className="font-bold">{title}</h1>
+            </div>
+
+            <div>
+              <ModeToggle />
+            </div>
           </header>
-          <main>
+          <main className="p-4">
             <Outlet />
           </main>
         </SidebarInset>
