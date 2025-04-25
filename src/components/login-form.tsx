d10e87093bc0a510e964c336447019db6022ff0e
Thu Nguyen <thuthien2001@gmail.com>
2025-04-25 11:49:45 +0700

    feat: login UI

---
+++ src/components/login-form.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { UI_LABELS } from "@/lib/routes";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Jack in for the good stuff</CardTitle>
+          <CardDescription>
+            Enter your creds to access the terminal.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  {UI_LABELS.login}
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router";
 import DashboardLayout from "@/layout/DashboardLayout";
-import Dashboard from "@/pages/Dashboard";
 import Products from "@/pages/Products";
 import Users from "@/pages/Users";
 import NotFound from "@/pages/NotFound";
@@ -8,22 +7,28 @@ import Categories from "@/pages/Categories";
 import Brands from "@/pages/Brands";
 
 import { APP_ROUTES } from "@/lib/routes";
+import Login from "@/pages/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/admin",
     element: <DashboardLayout />,
     children: [
-      { index: true, element: <Dashboard /> },
-      { path: APP_ROUTES.categories.url, element: <Categories /> },
+      // { index: true, element: <Dashboard /> },
+      { path: APP_ROUTES.categories.url, element: <Categories />, index: true },
       { path: APP_ROUTES.brands.url, element: <Brands /> },
       { path: APP_ROUTES.products.url, element: <Products /> },
       { path: APP_ROUTES.customers.url, element: <Users /> },
       { path: APP_ROUTES.orders.url },
-      { path: APP_ROUTES.login.url },
-      { path: APP_ROUTES.logout.url },
       { path: "me" },
     ],
+  },
+  {
+    path: APP_ROUTES.login.url,
+    element: <Login />,
+  },
+  {
+    path: APP_ROUTES.logout.url,
   },
   {
     path: "*",
