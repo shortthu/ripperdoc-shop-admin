3a1c8a6946f9acebd6d183c70ee0d1c1c3eb7d56
Thu Nguyen <thuthien2001@gmail.com>
2025-04-25 11:49:27 +0700

    fix: dark mode not applying to the whole app but only the dashboard UI

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { Outlet } from "react-router";
 import {
@@ -8,7 +8,6 @@ import {
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Separator } from "@radix-ui/react-separator";
-import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function DashboardLayout() {
@@ -19,26 +18,24 @@ export default function DashboardLayout() {
   };
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SidebarProvider>
-        <AppSidebar sendTitle={handleTitleChange} />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
-            <div className="flex items-center h-full">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <h1 className="font-bold">{title}</h1>
-            </div>
+    <SidebarProvider>
+      <AppSidebar sendTitle={handleTitleChange} />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+          <div className="flex items-center h-full">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <h1 className="font-bold">{title}</h1>
+          </div>
 
-            <div>
-              <ModeToggle />
-            </div>
-          </header>
-          <main className="p-4">
-            <Outlet />
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
+          <div>
+            <ModeToggle />
+          </div>
+        </header>
+        <main className="p-4">
+          <Outlet />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./router/index.tsx";
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
